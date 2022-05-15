e4cd0d37e522f7e5044770d92a61e6f6486cc231
David Valentine <david.valentine@umontreal.ca>
2022-05-15 01:29:11 -0400

    Mise à jour script de conversion

--- convertir-csv-ttl.py
+++ convertir-csv-ttl.py
@@ -1,10 +1,23 @@
-# conversion des termes de vocabulaire
-import csv, re
+#!/usr/bin/env python3
 
-vocabFile = 'vocab.csv'
+'''
+1. convertir les termes de vocabulaire : CSV -> TTL
+2. concaténer
+'''
+
+import csv, re, shutil
+
+# config
+vocabFile = 'vocab/vocab.csv'
+vocabFileTTL = 'vocab/vocab.ttl'
+TBox = 'qdmtl.ttl'
 
 # Initialisations
 extractedData = []
+
+'''
+1. convertir
+'''
 
 # Chargement des données
 with open(vocabFile, 'r+', newline = '') as csvFile:
@@ -12,7 +25,7 @@ with open(vocabFile, 'r+', newline = '') as csvFile:
         extractedData.append(dict(row))
 
 # Écriture du fichier Turtle
-with open('vocab.ttl', 'w') as ABox:
+with open(vocabFileTTL, 'w') as ABox:
     for subject in extractedData:
         for i, (property, value) in enumerate(subject.items()):
 
@@ -22,6 +35,7 @@ with open('vocab.ttl', 'w') as ABox:
             if 1 < i < len(subject):
                 assertions += ' ;\n'
 
+            # voir structure switch Python 3.10
             if property == 'URI':
                 assertions = value + ' rdf:type owl:NamedIndividual ,\n'
             elif property == 'rdf:type':
@@ -31,3 +45,11 @@ with open('vocab.ttl', 'w') as ABox:
 
         assertions += ' .\n\n'
         ABox.write(assertions)
+
+'''
+2. concaténer
+'''
+
+with open(TBox,'a') as file:
+    with open(vocabFileTTL,'r') as input:
+        shutil.copyfileobj(input, file)
